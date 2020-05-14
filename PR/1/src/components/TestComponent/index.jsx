c99e78ab6b843e30858528d9324a6b10c2eeeeaf
kellolo <87kell@gmail.com>
2020-05-14 20:20:25 +0200

    1

---
+++ PR/1/src/components/TestComponent/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './style.css';
+
+let test = () => {
+    return (
+        <div>
+            <h1>I am Your Father</h1>
+            <h2>- NNOOOOOOOO</h2>
+        </div>
+    )
+}
+
+export default test
