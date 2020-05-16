194c299ff1ee7f7ae251cd781a84d8a3cb3554d6
JohnCarmack117 <wiirus.mv@yandex.ru>
2020-05-16 22:06:55 +0300

    HomeTask#1
    comments deleted from jsx files

--- Students/Nikita_Karev/src/components/TestComponent/index.jsx
+++ Students/Nikita_Karev/src/components/TestComponent/index.jsx
@@ -5,11 +5,8 @@ import './style.css';
 import Message from '../Message.jsx';
 
 function test(props) {
-    // let name = this.props.father;
-    // let { father } = props;
-    let arr = ['Dart Vader', 'Chewbakka', 'Master Yoda'];
 
-    // let html = (<span><i>{ father } says:</i></span>);
+    let arr = ['Dart Vader', 'Chewbakka', 'Master Yoda'];
 
     let msgArr = arr.map(name => {
         return (<Message name={ name } />);
--- Students/Nikita_Karev/src/index.jsx
+++ Students/Nikita_Karev/src/index.jsx
@@ -4,14 +4,9 @@ import 'bootstrap';
 
 import './layout/style/main.css';
 
-// import TestComp from './components/TestComponent/index.jsx';
 import HomeTask_1 from './components/HomeTask_1/index.jsx';
 
 let container = document.getElementById('app')
-
-// ReactDom.render(
-//     <TestComp father={'Darth Vader'}/>
-//     , container);
   
 ReactDom.render(
     <HomeTask_1 />, 
