bf0e7054f1500576073911dcd791a470f7c7deb6
Sergey Belyakov <z0p@me.com>
2020-05-28 00:19:03 +1000

    update

--- Students/belyakov_sergey/src/store/reducers/room-reducer.js
+++ Students/belyakov_sergey/src/store/reducers/room-reducer.js
@@ -1,31 +1,18 @@
 import update from 'react-addons-update'
 
-import {SEND_MESSAGE} from '../actions/message-actions'
 import {ADD_ROOM} from "../actions/room-actions";
 
 const initialStore = {
   rooms: {
-    1: {title: 'Чат 1', messageList: []},
-    2: {title: 'Чат 2', messageList: []},
-    3: {title: 'Чат 3', messageList: []},
-    4: {title: 'Чат 4', messageList: []}
+    1: {title: 'Чат 1'},
+    2: {title: 'Чат 2'},
+    3: {title: 'Чат 3'},
+    4: {title: 'Чат 4'}
   }
 }
 
 export default function roomReducer(store = initialStore, action) {
   switch (action.type) {
-    case SEND_MESSAGE: {
-      return update(store, {
-        rooms: {
-          $merge: {
-            [action.roomId]: {
-              title: store.rooms[action.roomId].title,
-              messageList: [...store.rooms[action.roomId].messageList, action.messageId]
-            }
-          }
-        }
-      })
-    }
 
     case ADD_ROOM: {
       const roomId = Object.keys(store.rooms).length + 1;
@@ -34,7 +21,6 @@ export default function roomReducer(store = initialStore, action) {
           $merge: {
             [roomId]: {
               title: action.title,
-              messageList: []
             }
           }
         }
