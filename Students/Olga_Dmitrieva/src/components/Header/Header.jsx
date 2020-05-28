a50d2c25c101726bcb05411476e23e0a4aaca21b
Olga Dmitrieva <OlgaDmi1992@mail.ru>
2020-05-28 11:45:59 +0300

    Practice 4 is done

--- Students/Olga_Dmitrieva/src/components/Header/Header.jsx
+++ Students/Olga_Dmitrieva/src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './style.css';
 
 export default class Header extends Component {
+    static propTypes = {
+        chatId: PropTypes.number
+    }
+    static defaultProps = {
+        chatId: 2
+    }
+
     render() {
         return (
             <div className="header w-100">
-                <p>Chat App</p>
+                <p>Чат: { this.props.chatId }</p>
             </div>
         );
     }
--- Students/Olga_Dmitrieva/src/index.jsx
+++ Students/Olga_Dmitrieva/src/index.jsx
@@ -9,53 +9,22 @@ import { Provider } from 'react-redux';
 import initStore from './store/store.js';
 
 //components
-import Header from './components/Header/index.jsx';
-import ChatList from './components/ChatList/index.jsx';
-import MessagesField from './components/MessageField/MessageField.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Router from './router.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 let container = document.getElementById('app')
 
-let user = 'Loontik';
-
 ReactDom.render(
-    <Provider store = { initStore() }>
-        <div className = "d-flex w-100 justify-content-center">
-            <Header />
-        </div>
-        <div className = "d-flex w-100 justify-content-center">
-            <ChatList />
-            <MessagesField user={ user } />
-        </div>
-    </Provider>
+    <BrowserRouter>
+        <Provider store = { initStore() }>
+            <div className = "w-100 justify-content-center">
+                <MuiThemeProvider>
+                    <Router />
+                </MuiThemeProvider>
+            </div>
+        </Provider>
+    </BrowserRouter>
     ,
     container
 )
-
-// let messages = ['Hello', 'How are you?', 'I am fine'];
-
-// const handleClick = () => {
-//     messages.push('Ok');
-
-//     ReactDom.render(
-//         <div><MessageField messages={ messages }/></div>, 
-//         container
-//     )
-// }
-
-// const Message = props => <div>{ props.text }</div>
-
-// const MessageField = props => {
-//     let msgArr = props.messages.map(text => {
-//         return (<Message text={ text } />);
-//     });
-//     return (
-//         <div>
-//             <div>{ msgArr }</div>
-//             <button onClick={ handleClick }>Send</button>
-//         </div>
-//     )
-// }
-
-// ReactDom.render(
-//     <MessageField messages={ messages }/>
-//     , container);
